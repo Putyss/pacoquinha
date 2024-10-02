0c8fd1ffa5bb33f66d9505d177d0130d0875f96e
JHONATHAN EMILL QUITERIO DA ROCHA <jhonathan.eqrocha@senacsp.edu.br>
2024-10-02 20:38:38 -0300

    Commit inicial

---
+++ src/main/java/com/senac/uc6/pacoquinha/PacoquinhaApplication.java
@@ -0,0 +1,18 @@
+package com.senac.uc6.pacoquinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PacoquinhaApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ola Mundo!!!!");
+
+		SpringApplication.run(PacoquinhaApplication.class, args);
+
+	}
+
+}
